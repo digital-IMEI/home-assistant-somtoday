155c8e0f431bffef15370bb3f625bd7a856c0ca1
digital-IMEI <68896736+digital-IMEI@users.noreply.github.com>
2026-09-10 14:01:10 +0200

    Fix school selection and current Somtoday OAuth

--- custom_components/somtoday/const.py
+++ custom_components/somtoday/const.py
@@ -5,12 +5,15 @@ from datetime import timedelta
 DOMAIN = "somtoday"
 
 AUTH_BASE_URL = "https://somtoday.nl"
-ORGANIZATIONS_URL = "https://servers.somtoday.nl/organisaties.json"
+ORGANIZATIONS_URL = (
+    "https://raw.githubusercontent.com/NONtoday/organisaties.json/"
+    "refs/heads/main/organisaties.json"
+)
 AUTHORIZE_URL = f"{AUTH_BASE_URL}/oauth2/authorize"
 TOKEN_URL = f"{AUTH_BASE_URL}/oauth2/token"
 
-CLIENT_ID = "D50E0C06-32D1-4B41-A137-A9A850C892C2"
-REDIRECT_URI = "somtodayleerling://oauth/callback"
+CLIENT_ID = "somtoday-leerling-native"
+REDIRECT_URI = "somtoday://nl.topicus.somtoday.leerling/oauth/callback"
 
 CONF_ORGANIZATION = "organization"
 CONF_PROVIDER = "provider"
@@ -18,4 +21,3 @@ CONF_TOKEN = "token"
 
 UPDATE_INTERVAL = timedelta(minutes=15)
 SCHEDULE_DAYS = 14
-
